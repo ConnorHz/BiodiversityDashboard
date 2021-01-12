ad5cda2819b9097fc8a33f17da0e69acb90c31ff
ConnorHz <hertzconnor@gmail.com>
2021-01-12 17:34:25 -0600

    Complete data retrival and dropdown fill

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,17 @@
+// Retrieve Data
+d3.json("./data/samples.json").then(function(data) {
+        
+        // Fill Dropdown
+        d3.select("#selDataset").selectAll("option").data(data.names)
+            .enter()
+            .append("option")
+            .attr("value", d => {return d;})
+            .html(d => {return d;})
+    });
 
+
+
+
+
+// Fill Info Panel on Change
 
