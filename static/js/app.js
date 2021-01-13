9a92fe28725cbfc75e0c59f24bedf37dce48d5da
ConnorHz <hertzconnor@gmail.com>
2021-01-12 18:33:47 -0600

    Completed demographic panel

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,53 @@
-// Retrieve Data
-d3.json("./data/samples.json").then(function(data) {
+var dataPath = "./data/samples.json";
+
+// retrieve data
+d3.json(dataPath).then(function(data) {   
+        // fill dropdown
+    d3.select("#selDataset").selectAll("option").data(data.names)
+        .enter()
+        .append("option")
+        .attr("value", d => {return d})
+        .html(d => {return d});
+
+    // run option changed on data load to update the dashboard
+    optionChanged(d3.select("#selDataset").property("value"));
+
+});
+
+/**
+ * Updates the paragraph elements within the "Demographic Info" div
+*/
+function updateMetadataPanel(data) {  
+    var metadataSelection = d3.select("#sample-metadata").selectAll("p")
+        .data(Object.entries(data));
+
+    metadataSelection.enter()
+        .append("p")
+        .merge(metadataSelection)
+        .text(d => {return `${d[0]}: ${d[1]}`});
+
+    metadataSelection.exit().remove();
+}
+
+function updateBar(data) {  
+ 
+}
+
+function optionChanged(subjectId) {
+    d3.json(dataPath).then(function(data) {
+
+        // get metadata for selected test subject
+        metadata = data.metadata.find(({id}) => id === parseInt(subjectId));
         
-        // Fill Dropdown
-        d3.select("#selDataset").selectAll("option").data(data.names)
-            .enter()
-            .append("option")
-            .attr("value", d => {return d;})
-            .html(d => {return d;})
+        console.log(metadata);
+
+        updateMetadataPanel(metadata);
     });
+}
 
 
 
 
 
-// Fill Info Panel on Change
+
 
